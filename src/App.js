98e65f76991ae896bf7c130937b2ed2a2d35fb91
Adi Carlisle <adicarlisle@gmail.com>
2022-05-10 02:14:41 +0100

    render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from "./logo.svg";
+import React, { Fragment, useState, useEffect } from 'react';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [data, setData] = useState(null);
+  const [value, setValue] = useState("");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        'https://vfmig4xbzd.execute-api.eu-west-2.amazonaws.com/default', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ websiteName: "https://" + value })}
+      );
+      const response = await result.json();
+      setData(response);
+    };
+    
+    
+    fetchData();
+    
+  }, []);
+ 
+  return (
+    <Fragment>
+      <header className="App-header">
+
+        
+        <img className="App-logo" src={Logo} alt="react logo"/>
+        
+      <div>
+        
+        <label>Website: </label>
+        <input type="text" id="value" name="value" value={value} onChange={ e => setValue(e.target.value) }/>
+        
+        
+        </div>
+      <h6 style={{color:"yellow"}}>{JSON.stringify(data,null,2)}</h6>
+      </header>
+      
+    
+      
+    </Fragment>
+  );
+        }
 export default App;
